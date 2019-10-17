d507fe3ab34d4876a63ec2659cee132c8de7a097
RichDoherty <doherty622@gmail.com>
2019-10-17 15:02:00 -0400

    2 files modified

--- IInputHandler.ts
+++ IInputHandler.ts
@@ -5,10 +5,15 @@ import SnakeController from './SnakeController'
 import Player from './Player';
 import View from './IView';
 
+/** Interface that handles inputs */
 interface InputHandler {
+  /** Indicates if the player has moved the snake left. */
   madeLeftMove: () => boolean;
+  /** Indicates if the player has moved the snake right. */
   madeRightMove: () => boolean;
+  /** Resets the state of the input. */
   resetLeftMove: () => void;
+  /** Resets the state of the input. */
   resetRightMove: () => void;
 }
 
--- LRInputHandler.ts
+++ LRInputHandler.ts
@@ -6,9 +6,11 @@ import Player from './Player';
 import View from './IView';
 import InputHandler from './iInputHandler';
 
+/** Implemets InputHandler to be used by the player */
 class LRInputHandler implements InputHandler {
   private wasLeftArrowPushed: boolean;
   private wasRightArrowPushed: boolean;
+  /** Initializes the states of the button inputs and checks if the player inputs a command for the snake to move. */
   constructor() {
     this.wasLeftArrowPushed = false;
     this.wasRightArrowPushed = false;
@@ -17,12 +19,15 @@ class LRInputHandler implements InputHandler {
       else if(event.key === "ArrowRight") { this.wasRightArrowPushed = true; }
     })
   }
+  /** Returns true or fals depending if the player made an input to turn left. */
   public madeLeftMove() {
     return this.wasLeftArrowPushed;
   }
+    /** Returns true or fals depending if the player made an input to turn right. */
   public madeRightMove() {
     return this.wasRightArrowPushed;
   }
+  /** Resets  */
   public resetLeftMove() {
     this.wasLeftArrowPushed = false;
   }
