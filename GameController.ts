dcb42c7beb7c6aaed3e82e6812d07bba7926aac2
RichDoherty <doherty622@gmail.com>
2019-10-17 14:17:00 -0400

    2 files modified

--- GameController.ts
+++ GameController.ts
@@ -4,20 +4,35 @@ import WorldModel from './WorldModel';
 import SnakeController from './SnakeController'
 import Player from './Player';
 
+/** Class that sets players and runs the game */
 class GameController {
   private worldModel:WorldModel;
   private p1: Player;
   private p2: Player;
-  
+  /**
+   * Initiaizes the game world.
+   * @param w - WorldModel that is being used.
+   */
   constructor(w:WorldModel) {
     this.worldModel = w;
   }
+  /**
+   * Sets Player 1 as a human player or ai.
+   * @param newPlayer - What player1 will be set to.
+   */
   public set player1(newPlayer:Player) {
     this.p1 = newPlayer;
   }
+  /**
+   * Sets Player 2 as a human player or ai.
+   * @param newPlayer - What player1 will be set to.
+   */
   public set player2(newPlayer:Player) {
     this.p2 = newPlayer;
   }
+  /**
+   * Runs the game.
+   */
   public run() {
     let lastTime = 0;
     let updateFrame = milliseconds => {
--- HumanPlayer.ts
+++ HumanPlayer.ts
@@ -5,14 +5,23 @@ import SnakeController from './SnakeController';
 import Player from './Player';
 import InputHandler from './iInputHandler';
 
+/** Class representing a human player */
 class HumanPlayer extends Player {
   private controller:SnakeController;
   private handler:InputHandler;
+  /**
+   * Creates a controller for the player to use.
+   * @param sc - SnakeController
+   * @param ih - InputHandler
+   */
   constructor(sc:SnakeController, ih:InputHandler) {
     super(sc);
     this.controller = sc;
     this.handler = ih;
   }
+  /**
+   * Makes the snake turn when the player makes an input.
+   */
   makeTurn():void {
     if(this.handler.madeLeftMove) {
       this.controller.turnSnakeLeft;
