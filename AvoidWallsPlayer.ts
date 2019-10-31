914d72d1b0c941a4290948bae6f506c94a404c97
RichDoherty <doherty622@gmail.com>
2019-10-31 15:13:18 -0400

    Updated AvoidWallsPlayer

--- AvoidWallsPlayer.ts
+++ AvoidWallsPlayer.ts
@@ -9,10 +9,38 @@ class AvoidWallsPlayer extends Player {
     super(controller);
   }
   public makeTurn() {
-    if(this.sc.snakeDirection === 2 && this.sc.snakePosition.x === 0 && this.sc.snakePosition.y < this.sc.height) { this.sc.turnSnakeLeft(); }
-    else if(this.sc.snakeDirection === 1 && this.sc.snakePosition.y === this.sc.height && this.sc.snakePosition.x < this.sc.width) { this.sc.turnSnakeLeft(); }
-    else if(this.sc.snakeDirection === 3 && this.sc.snakePosition.x === this.sc.width && this.sc.snakePosition.y < this.sc.height) { this.sc.turnSnakeLeft(); }
-    else if(this.sc.snakeDirection === 4 && this.sc.snakePosition.y === 0 && this.sc.snakePosition.x < this.sc.height) { this.sc.turnSnakeLeft(); }
+    if(this.sc.snakeDirection === 2 && this.sc.snakePosition.x === 0) {
+      if(this.sc.snakePosition.y < this.sc.height/2) {
+        this.sc.turnSnakeRight(); 
+        }
+      else {
+        this.sc.turnSnakeLeft();
+        }
+      }
+    else if(this.sc.snakeDirection === 3 && this.sc.snakePosition.x === this.sc.width) {
+        if(this.sc.snakePosition.y < this.sc.height/2) {
+          this.sc.turnSnakeLeft();
+        }
+      else {
+        this.sc.turnSnakeRight();
+      }
+    }
+    else if(this.sc.snakeDirection === 1 && this.sc.snakePosition.y === this.sc.height) {
+      if(this.sc.snakePosition.x < this.sc.width/2) {
+        this.sc.turnSnakeLeft();
+      }
+      else {
+        this.sc.turnSnakeRight();
+      }
+    }
+    else if(this.sc.snakeDirection === 4 && this.sc.snakePosition.y === 0) {
+      if(this.sc.snakePosition.x < this.sc.width/2) {
+        this.sc.turnSnakeRight();
+      }
+      else {
+        this.sc.turnSnakeLeft();
+      }
+    }
   }
 }
 
--- Snake.ts
+++ Snake.ts
@@ -35,18 +35,18 @@ class Snake {
     else { this.currentParts[0] = new Point(this.currentParts[0].x, this.currentParts[0].y - numOfSquares) }
   }
   /**
-   * Turns the snake left.
+   * Turns the snake right.
    */
-  public turnLeft() {
+  public turnRight() {
     if(this.currentDirection === 1) { this.currentDirection = 2 }
     else if(this.currentDirection === 2) { this.currentDirection = 4 }
     else if(this.currentDirection === 3) { this.currentDirection = 1 }
     else { this.currentDirection = 3 }
   }
   /**
-   * Turns the snake right.
+   * Turns the snake left.
    */
-  public turnRight() {
+  public turnLeft() {
     if(this.currentDirection === 1) { this.currentDirection = 3 }
     else if(this.currentDirection === 2) { this.currentDirection = 1 }
     else if(this.currentDirection === 3) { this.currentDirection = 4 }
